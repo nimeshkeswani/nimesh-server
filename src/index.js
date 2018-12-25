76d062caf0cbb2363532839b31148c060c345743
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-12-25 13:46:02 -0800

    initialize sequelize config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { sequelize } = require('./sequelize/models/index')
 
 // Constants
 const PORT = 8080
@@ -12,3 +13,13 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, HOST)
 console.log(`Running on http://${HOST}:${PORT}`)
+
+// Test Sequelize connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Sequilize connection to postgres has been established successfully.')
+  })
+  .catch(err => {
+    console.error('Unable to establish Sequilize connection to the postgres:', err)
+  })
