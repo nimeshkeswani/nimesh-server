141fdc267f5a86706c2c15729a6a4c55f2de2c4a
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-12-25 13:46:15 -0800

    add User model

---
+++ src/sequelize/models/user.js
@@ -0,0 +1,40 @@
+'use strict'
+const bcrypt = require('bcrypt')
+
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      set: function (val) {
+        this.setDataValue('password', this.generateHash(val))
+      }
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+    updatedAt: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    }
+  }, {
+    instanceMethods: {
+      generateHash: password => bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+    }
+  })
+
+  return User
+}
